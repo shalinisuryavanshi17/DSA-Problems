13618b40e4e1545fb4cf1bdb5ce25f558a1bbef7
G <gajbinakr.shalini0@gmail.com>
2021-06-06 19:14:35 +0530

    refactored to efficient code

--- MoveAllNegEles.java
+++ MoveAllNegEles.java
@@ -6,37 +6,55 @@ Examples :
 Input: -12, 11, -13, -5, 6, -7, 5, -3, -6
 Output: -12 -13 -5 -7 -3 -6 11 6 5
  */
+
 import java.util.*;
 
 class MoveAllNegEles {
-    public static void moveNegelements(int arr[]) {
-        Queue<Integer> negStack = new LinkedList<>();
-        for (int i : arr) {
-            if (i < 0) {
-                negStack.offer(i);
+    public static void moveNegelements(int arr[],int start,int end) {
+       if(start<end)
+       {
+           int pivot_index=partition(arr,start,end);
+           moveNegelements(arr,start,pivot_index-1);
+           moveNegelements(arr, pivot_index+1 ,end);
+       }
+       else{
+           return;
+       }
+
+    }
+    public static int partition(int arr[],int start,int end)
+    {
+        int pivot=arr[end];
+        int p_index=start;
+        for (int i = start; i < end; i++) {
+            if(arr[i]<=pivot)
+            {
+                swap(arr,p_index,i);
+                p_index++;
             }
+            
         }
-        while (!negStack.isEmpty()) {
-            System.out.print(negStack.poll() + " ");
-        }
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] > 0) {
-                System.out.print(arr[i] + " ");
-            }
-        }
+        swap(arr,p_index,end);
+        return p_index;
+    }
+    public  static void swap(int arr[],int i,int p_index)
+    {
+        int temp=arr[p_index];
+        arr[p_index]=arr[i];
+        arr[i]=temp;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int arr[] = new int[n];
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < arr.length; i++) {
             arr[i] = sc.nextInt();
         }
-        Arrays.sort(arr);
-        System.out.println(Arrays.toString(arr));
         sc.close();
-        //moveNegelements(arr);
-
+        moveNegelements(arr,0,arr.length-1);
+          for (int i = 0; i < arr.length; i++) {
+              System.out.print(arr[i]+" ");
+          }
     }
 }
